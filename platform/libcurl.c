90c1fb09059583a512f180d4da0b85a243f7bbcc
45gfg9 <45gfg9@45gfg9.net>
2025-07-24 14:43:02 +0800

    compat(curl): do not use tmpfile

--- platform/libcurl.c
+++ platform/libcurl.c
@@ -6,19 +6,43 @@
 #include <errno.h>
 #include <curl/curl.h>
 
+struct curl_string {
+  char *ptr;
+  size_t len;
+};
+
 static void init_curl(void) __attribute__((constructor));
 static void init_curl(void) {
   curl_global_init(CURL_GLOBAL_DEFAULT);
 }
 
-char *request_get(const char *url) {
-  FILE *resp_file = tmpfile();
-  if (!resp_file) {
-    return NULL;
+static void cleanup_curl(void) __attribute__((destructor));
+static void cleanup_curl(void) {
+  curl_global_cleanup();
+}
+
+static void init_string(struct curl_string *s) {
+  s->len = 0;
+  s->ptr = calloc(1, 1); // initial null-terminated string
+}
+
+static size_t writefunc(char *ptr, size_t size, size_t nmemb, struct curl_string *s) {
+  size_t new_len = s->len + size * nmemb;
+  char *new_ptr = realloc(s->ptr, new_len + 1);
+  if (new_ptr == NULL) {
+    errno = ENOMEM;
+    return 0; // cause curl to fail
   }
 
-  char *resp_buf = NULL;
+  s->ptr = new_ptr;
+  memcpy(s->ptr + s->len, ptr, size * nmemb);
+  s->ptr[new_len] = '\0';
+  s->len = new_len;
 
+  return size * nmemb;
+}
+
+char *request_get(const char *url) {
   CURL *curl_handle = curl_easy_init();
   if (!curl_handle) {
     // https://curl.se/libcurl/c/curl_easy_init.html
@@ -27,28 +51,24 @@ char *request_get(const char *url) {
     goto end;
   }
 
+  struct curl_string resp_string;
+  init_string(&resp_string);
+
   curl_easy_setopt(curl_handle, CURLOPT_URL, url);
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, resp_file);
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, writefunc);
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &resp_string);
 
   CURLcode res = curl_easy_perform(curl_handle);
   curl_easy_cleanup(curl_handle);
   if (res != CURLE_OK) {
+    fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
     errno = EIO; // I/O error
     goto end;
   }
 
-  long resp_size = ftell(resp_file);
-  resp_buf = malloc(resp_size + 1);
-  if (!resp_buf) {
-    errno = ENOMEM; // out of memory
-    goto end;
-  }
-
-  rewind(resp_file);
-  fread(resp_buf, 1, resp_size, resp_file);
-  resp_buf[resp_size] = '\0';
+  return resp_string.ptr;
 
 end:
-  fclose(resp_file);
-  return resp_buf;
+  free(resp_string.ptr);
+  return NULL;
 }
