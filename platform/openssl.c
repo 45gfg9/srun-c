2037058d9b58792e79c62430afe78cf5756c79eb
45gfg9 <45gfg9@45gfg9.net>
2025-08-12 23:55:44 +0800

    compat(openssl): simplify code

--- platform/openssl.c
+++ platform/openssl.c
@@ -6,7 +6,6 @@
 
 #include "compat.h"
 #include <stdlib.h>
-#include <string.h>
 #include <openssl/evp.h>
 #include <openssl/sha.h>
 #include <openssl/hmac.h>
@@ -18,11 +17,6 @@ inline size_t sha1_digest(const uint8_t *data, size_t len, uint8_t digest[static
 
 inline size_t hmac_md5_digest(const uint8_t *key, size_t key_len, const uint8_t *data, size_t data_len,
                               uint8_t digest[static 16]) {
-  unsigned int md_len = 16;
-  HMAC(EVP_md5(), key, (int)key_len, data, data_len, digest, &md_len);
-  if (md_len != 16) {
-    errno = EINVAL; // Invalid length
-    return 0;
-  }
+  HMAC(EVP_md5(), key, (int)key_len, data, data_len, digest, NULL);
   return 16;
 }
