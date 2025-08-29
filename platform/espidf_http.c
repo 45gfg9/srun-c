114edd6d48cf088adabb562dcc2787e6ccd10457
45gfg9 <45gfg9@45gfg9.net>
2025-08-29 22:52:34 +0800

    compat(espidf_http): rewrite using event handler

--- platform/espidf_http.c
+++ platform/espidf_http.c
@@ -13,65 +13,67 @@
 #include <esp_http_client.h>
 #include <esp_crt_bundle.h>
 
-// TODO: rewrite using event handler
-
-char *request_get_body(const_srun_handle handle, const char *url) {
-  char *response = NULL;
+static void request(const_srun_handle handle, const char *url, http_event_handle_cb evt_handler, void *user_data) {
   esp_http_client_config_t config = {
       .url = url,
-      .method = HTTP_METHOD_GET,
       .cert_pem = handle->cacert_pem,
       .cert_len = 0, // auto detect
-      .crt_bundle_attach = esp_crt_bundle_attach,
+      .method = HTTP_METHOD_GET,
       .disable_auto_redirect = true,
+      .event_handler = evt_handler,
+      .user_data = user_data,
+      .use_global_ca_store = true,
+      .crt_bundle_attach = esp_crt_bundle_attach,
   };
 
   esp_http_client_handle_t client = esp_http_client_init(&config);
-  if (esp_http_client_open(client, 0) == ESP_OK) {
-    int content_length = esp_http_client_fetch_headers(client);
-    if (content_length > 0) {
-      response = (char *)malloc(content_length + 1);
-      if (response) {
-        int read_len = esp_http_client_read_response(client, response, content_length);
-        if (read_len >= 0) {
-          response[read_len] = '\0';
-        } else {
-          free(response);
-          response = NULL;
-        }
-      }
-    }
-  }
-
+  esp_http_client_perform(client);
   esp_http_client_cleanup(client);
-  return response;
+}
+
+struct get_body_response {
+  int content_length;
+  char *data;
+};
+
+esp_err_t request_get_body_event_handler(esp_http_client_event_t *evt) {
+  struct get_body_response *response = evt->user_data;
+  if (evt->event_id == HTTP_EVENT_ON_HEADER && !strcasecmp(evt->header_key, "Content-Length")) {
+    response->content_length = (int)strtol(evt->header_value, NULL, 10);
+  } else if (evt->event_id == HTTP_EVENT_ON_DATA) {
+    if (response->data == NULL) {
+      response->data = malloc(response->content_length + 1);
+      if (response->data == NULL) {
+        return ESP_FAIL;
+      }
+      response->data[0] = '\0';
+    }
+    strncat(response->data, evt->data, evt->data_len);
+  }
+  return ESP_OK;
+}
+
+char *request_get_body(const_srun_handle handle, const char *url) {
+  struct get_body_response response = {};
+  request(handle, url, request_get_body_event_handler, &response);
+  return response.data;
+}
+
+static esp_err_t request_get_location_event_handler(esp_http_client_event_t *evt) {
+  if (evt->event_id == HTTP_EVENT_ON_HEADER && !strcasecmp(evt->header_key, "Location")) {
+    char **pserver = evt->user_data;
+    if (*pserver) {
+      free(*pserver);
+    }
+    *pserver = strdup(evt->header_value);
+  }
+  return ESP_OK;
 }
 
 char *request_get_location(const_srun_handle handle, const char *url) {
-  char *response = NULL;
-  esp_http_client_config_t config = {
-      .url = url,
-      .method = HTTP_METHOD_GET,
-      .cert_pem = handle->cacert_pem,
-      .cert_len = 0, // auto detect
-      .crt_bundle_attach = esp_crt_bundle_attach,
-      .disable_auto_redirect = true,
-  };
-
-  esp_http_client_handle_t client = esp_http_client_init(&config);
-  if (esp_http_client_open(client, 0) == ESP_OK) {
-    esp_http_client_fetch_headers(client);
-    int status_code = esp_http_client_get_status_code(client);
-    if (status_code >= 300 && status_code < 400) {
-      char *location = NULL;
-      if (esp_http_client_get_header(client, "Location", &location) == ESP_OK && location) {
-        response = strdup(location);
-      }
-    }
-  }
-
-  esp_http_client_cleanup(client);
-  return response;
+  char *server = NULL;
+  request(handle, url, request_get_location_event_handler, &server);
+  return server;
 }
 
 #endif
